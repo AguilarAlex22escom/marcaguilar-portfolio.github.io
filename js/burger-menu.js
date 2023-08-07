889afd921cba067171a9d48cb6789f20f19a7e12
Marc Alexander Aguilar <81877369+AguilarAlex22escom@users.noreply.github.com>
2023-08-07 09:05:07 -0600

    Add files via upload

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,8 +1,10 @@
 let burgerButton = document.querySelector(".burger-button");
-let menuOptions = document.querySelector(".responsive-list");
+let menuOptions = document.querySelector(".responsive-menu");
+let header = document.querySelector("header");
 
 // let links = document.querySelectorAll(".responsive-menu ul li a");
 burgerButton.addEventListener("click", () => {
   burgerButton.classList.toggle("actived");
   menuOptions.classList.toggle("actived");
+  header.classList.toggle("actived");
 });
